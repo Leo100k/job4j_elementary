5262bd8ac3deca5255bac1ebcefb3a69d37fd51f
Leo100k <lemail2004@mail.ru>
2023-09-26 12:17:16 +0300

    3.3.Фиксация изменений на githab [17]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -7,16 +7,16 @@ public class Fit {
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 100) * 1.15;
+        double rsl = (height - 110) * 1.15;
         return rsl;
     }
 
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
-        height = 160;
+        height = 167;
         double woman = Fit.womanWeight(height);
         System.out.println("Man 187 is " + man);
-        System.out.println("Woman 187 is " + woman);
+        System.out.println("Woman 167 is " + woman);
     }
 }
