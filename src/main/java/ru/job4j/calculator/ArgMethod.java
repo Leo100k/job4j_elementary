8909f0ecce81197c82d0d3a5511d69eabb31865a
Leo100k <lemail2004@mail.ru>
2023-09-26 13:12:40 +0300

    3.3.Фиксация изменений на githab [19]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -1,8 +1,7 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    // x
-    public static void hello(String name, int age) {
+        public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
