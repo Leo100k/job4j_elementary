7847fd6f3826a696e30d23c348d1dd475af35bd8
Leo100k <lemail2004@mail.ru>
2023-09-26 15:05:01 +0300

    3.3.Фиксация изменений на githab [21]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -12,8 +12,8 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
         float usd = Converter.rubleToDollar(180);
+        float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("180 rubles are " + usd + " usd.");
     }
